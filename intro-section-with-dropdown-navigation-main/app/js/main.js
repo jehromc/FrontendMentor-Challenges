748c0020c487abce94f52e4f57bd739c17e9107e
jaycee altamia <jayceealtamia@gmail.com>
2023-06-28 13:10:08 +0800

    toggle up and down

--- intro-section-with-dropdown-navigation-main/app/js/main.js
+++ intro-section-with-dropdown-navigation-main/app/js/main.js
@@ -5,6 +5,10 @@ const openMenu = document.querySelector('.nav__menu--open');
 const navMenu = document.querySelector('[data-nav-menu]');
 const mobileBackdrop = document.querySelector('.mobile-backdrop');
 
+const navSubMenu = document.querySelector('[data-sub-list]');
+const arrowDown = document.querySelector('[data-arrow-down]');
+const arrowUp = document.querySelector('[data-arrow-up]');
+
 //shows  menu by adding the  class
 const openMenuHandler = () => {
   navMenu.classList.add('nav__container--show');
@@ -15,5 +19,22 @@ const closeMenuHandler = () => {
   navMenu.classList.remove('nav__container--show');
   mobileBackdrop.classList.remove('mobile-backdrop--show');
 };
+
+//toggle sub menu
+const arrowDownHandler = () => {
+  toggleArrow();
+};
+const arrowUpHandler = () => {
+  toggleArrow();
+};
+
+function toggleArrow() {
+  navSubMenu.classList.toggle('nav__list__sub-container-hide');
+  arrowUp.classList.toggle('arrow--hidden');
+  arrowDown.classList.toggle('arrow--hidden');
+}
+
 openMenu.addEventListener('click', openMenuHandler);
 closeMenu.addEventListener('click', closeMenuHandler);
+arrowDown.addEventListener('click', arrowDownHandler);
+arrowUp.addEventListener('click', arrowUpHandler);
