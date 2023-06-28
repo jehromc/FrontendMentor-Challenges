652dc071b7d657e42a2181cec8a4ed1f28986c10
jaycee altamia <jayceealtamia@gmail.com>
2023-06-29 03:55:58 +0800

    Desktop Design

--- intro-section-with-dropdown-navigation-main/app/js/main.js
+++ intro-section-with-dropdown-navigation-main/app/js/main.js
@@ -9,6 +9,10 @@ const navSubMenu = document.querySelector('[data-sub-list]');
 const arrowDown = document.querySelector('[data-arrow-down]');
 const arrowUp = document.querySelector('[data-arrow-up]');
 
+const arrowDownCompany = document.querySelector('[data-arrow-down-company ]');
+const arrowUpCompany = document.querySelector('[data-arrow-up-company ]');
+const navSubMenuCompany = document.querySelector('[data-sub-list-company]');
+
 //shows  menu by adding the  class
 const openMenuHandler = () => {
   navMenu.classList.add('nav__container--show');
@@ -20,21 +24,38 @@ const closeMenuHandler = () => {
   mobileBackdrop.classList.remove('mobile-backdrop--show');
 };
 
-//toggle sub menu
+//toggle sub menu for features
 const arrowDownHandler = () => {
   toggleArrow();
 };
 const arrowUpHandler = () => {
   toggleArrow();
 };
-
 function toggleArrow() {
   navSubMenu.classList.toggle('nav__list__sub-container-hide');
   arrowUp.classList.toggle('arrow--hidden');
   arrowDown.classList.toggle('arrow--hidden');
 }
 
+//toggle Sub Menu for Company link
+const arrowDownCompanyHandler = () => {
+  navSubMenuCompany.classList.toggle('nav__list__sub-container-hide');
+  arrowUpCompany.classList.toggle('arrow--hidden');
+  arrowDownCompany.classList.toggle('arrow--hidden');
+};
+
+const arrowUpCompanyHandler = () => {
+  navSubMenuCompany.classList.toggle('nav__list__sub-container-hide');
+  arrowUpCompany.classList.toggle('arrow--hidden');
+  arrowDownCompany.classList.toggle('arrow--hidden');
+};
+
+
+//adding event listener for all clickable link
 openMenu.addEventListener('click', openMenuHandler);
 closeMenu.addEventListener('click', closeMenuHandler);
 arrowDown.addEventListener('click', arrowDownHandler);
 arrowUp.addEventListener('click', arrowUpHandler);
+
+arrowDownCompany.addEventListener('click', arrowDownCompanyHandler);
+arrowUpCompany.addEventListener('click', arrowUpCompanyHandler);
