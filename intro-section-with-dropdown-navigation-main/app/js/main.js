bd76185a9a685f07473c0f29e2f1fa20dc1215f3
jaycee altamia <jayceealtamia@gmail.com>
2023-06-28 10:52:28 +0800

    update  design

--- intro-section-with-dropdown-navigation-main/app/js/main.js
+++ intro-section-with-dropdown-navigation-main/app/js/main.js
@@ -3,14 +3,17 @@ console.log('Intro Section challenge');
 const closeMenu = document.querySelector('.nav__menu--close');
 const openMenu = document.querySelector('.nav__menu--open');
 const navMenu = document.querySelector('[data-nav-menu]');
+const mobileBackdrop = document.querySelector('.mobile-backdrop');
 
 //shows  menu by adding the  class
 const openMenuHandler = () => {
   navMenu.classList.add('nav__container--show');
+  mobileBackdrop.classList.add('mobile-backdrop--show');
 };
 //hide   menu by removing the  class
 const closeMenuHandler = () => {
   navMenu.classList.remove('nav__container--show');
+  mobileBackdrop.classList.remove('mobile-backdrop--show');
 };
 openMenu.addEventListener('click', openMenuHandler);
 closeMenu.addEventListener('click', closeMenuHandler);
