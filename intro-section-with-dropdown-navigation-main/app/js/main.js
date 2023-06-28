eaa1bdc37827fe88cb1f6237299df829fb4a4846
jaycee altamia <jayceealtamia@gmail.com>
2023-06-28 09:29:54 +0800

    adding nav container

--- intro-section-with-dropdown-navigation-main/app/js/main.js
+++ intro-section-with-dropdown-navigation-main/app/js/main.js
@@ -1,2 +1,16 @@
 import '../scss/main.scss';
 console.log('Intro Section challenge');
+const closeMenu = document.querySelector('.nav__menu--close');
+const openMenu = document.querySelector('.nav__menu--open');
+const navMenu = document.querySelector('[data-nav-menu]');
+
+//shows  menu by adding the  class
+const openMenuHandler = () => {
+  navMenu.classList.add('nav__container--show');
+};
+//hide   menu by removing the  class
+const closeMenuHandler = () => {
+  navMenu.classList.remove('nav__container--show');
+};
+openMenu.addEventListener('click', openMenuHandler);
+closeMenu.addEventListener('click', closeMenuHandler);
